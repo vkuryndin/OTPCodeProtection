fd5caed91d46fb2c3f9ef2be41ada85b807ab041
kurindinvv <kurindinvv@ERV-W0013>
2026-04-26 10:57:23 +0300

    moved to record

--- src/main/java/org/example/dto/LoggedInUserResponse.java
+++ src/main/java/org/example/dto/LoggedInUserResponse.java
@@ -4,52 +4,11 @@ import org.example.model.Role;
 
 import java.time.LocalDateTime;
 
-public class LoggedInUserResponse {
-    private Long userId;
-    private String login;
-    private Role role;
-    private LocalDateTime loggedInAt;
-    private LocalDateTime expiresAt;
-
-
-    public LoggedInUserResponse(Long userId, String login, Role role,
-                                LocalDateTime loggedInAt, LocalDateTime expiresAt) {
-        this.userId = userId;
-        this.login = login;
-        this.role = role;
-        this.loggedInAt = loggedInAt;
-        this.expiresAt = expiresAt;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public LocalDateTime getExpiresAt() {
-        return expiresAt;
-    }
-
-    public void setExpiresAt(LocalDateTime expiresAt) {
-        this.expiresAt = expiresAt;
-    }
+public record LoggedInUserResponse(
+        Long userId,
+        String login,
+        Role role,
+        LocalDateTime loggedInAt,
+        LocalDateTime expiresAt
+) {
 }
